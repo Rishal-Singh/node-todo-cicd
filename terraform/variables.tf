e8e6c64f55f3401e1e1b7d57ec2f88d52441ad31
Rishal Singh <65354251+Rishal-Singh@users.noreply.github.com>
2026-05-28 15:12:32 +0530

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "key_name" {
+  description = "AWS EC2 Key Pair Name"
+}
+
+variable "ami_id" {
+  default = "ami-091138d0f0d41ff90"
+}
