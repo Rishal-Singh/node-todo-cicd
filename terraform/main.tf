747bc739850f50eecfdfab5291026c543e9e93a1
Rishal Singh <65354251+Rishal-Singh@users.noreply.github.com>
2026-05-28 15:08:06 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,18 +1,73 @@
-resource "docker_image" "todo_image" {
-	name = "trainwithshubham/todo-app-node:latest"
-	keep_locally = false
+resource "aws_security_group" "node_todo_sg" {
+  name = "node-todo-sg"
+
+  ingress {
+    description = "SSH Access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Application Port"
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Jenkins Port"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SonarQube Port"
+    from_port   = 9000
+    to_port     = 9000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
-resource "docker_container" "todo_container" {
-	image = docker_image.todo_image.name
-	name = "todoapp-container"
-	ports {
-		internal = 8000
-		external = 8000
-	}
+resource "aws_instance" "node_todo_server" {
 
-	depends_on = [
-		docker_image.todo_image
-	]
-	
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.node_todo_sg.id]
+
+  root_block_device {
+    volume_size = 20
+    volume_type = "gp3"
+  }
+
+  tags = {
+    Name = "Node-Todo-App-Server"
+  }
+
+  user_data = <<-EOF
+              #!/bin/bash
+
+              apt update -y
+
+              apt install docker.io -y
+              systemctl start docker
+              systemctl enable docker
+
+              apt install docker-compose -y
+
+              usermod -aG docker ubuntu
+              EOF
 }
